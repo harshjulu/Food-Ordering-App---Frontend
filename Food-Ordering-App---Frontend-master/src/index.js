6bfd5cfeec59ad84cd8ddf480bca98120e633c15
harshjulu <32120633+harshjulu@users.noreply.github.com>
2020-11-23 09:36:56 +0530

    Add files via upload

---
+++ Food-Ordering-App---Frontend-master/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import Home from './screens/home/Home';
+import Controller from './screens/Controller';
+
+ReactDOM.render(
+    <Controller />,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
